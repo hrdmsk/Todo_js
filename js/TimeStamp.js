7ad357ba0932290bda6cbc76fa9177e34cbfe23d
hankakukatakana <masaki.0409.a@gmail.com>
2022-12-29 12:49:36 +0900

    komi

---
+++ js/TimeStamp.js
@@ -0,0 +1,67 @@
+"use strict";
+
+const todoLists = document.getElementById("timeLists");
+const addBtn = document.getElementById("addBtn");
+const allDel = document.getElementById("allDel")
+
+let currentNum = 1;
+let times = [];
+const DB = localStorage
+var now = new Date()
+
+var YY = now.getFullYear
+var MM = now.getMonth
+var DD = now.getDay
+var hh = now.getHours
+var mm = now.getMinutes
+var ss = now.getSeconds
+
+addBtn.addEventListener("click", () => {
+  }
+
+  times.push({
+    id: currentNum,
+    title: 
+  });
+
+  createListView();
+
+  inputTodo.value = "";
+  currentNum++;
+);
+
+allDel.addEventListener("click",() =>{
+  todos.length = 0;
+  createListView();
+});
+
+const createListView = () => {
+  // タスクを描画するときにtbodyの中に子要素が一つでもあれば一つになるまで削除する
+  while (todoLists.firstChild) {
+    todoLists.removeChild(todoLists.firstChild);
+  }
+
+  todos.forEach((todo) => {
+    // // tr要素の生成
+    let todoItem = document.createElement("tr");
+    // todoのIDを表示するthの生成
+    const todoId = document.createElement("th");
+    // todoのタイトルを表示するthの生成
+    const todoTitle = document.createElement("th");
+    // 削除ボタンを表示するthの生成
+    const todoDelete = document.createElement("th");
+    // 削除ボタンの生成
+    const deleteBtn = document.createElement("button");
+
+    todoId.textContent = todo.id;
+    todoTitle.textContent = todo.title;
+    deleteBtn.textContent = "削除";
+    deleteBtn.classList.add("btn", "btn-secondary");
+    todoDelete.appendChild(deleteBtn);
+
+    todoItem.appendChild(todoId);
+    todoItem.appendChild(todoTitle);
+    todoItem.appendChild(todoDelete);
+    todoLists.appendChild(todoItem);
+  });
+};
